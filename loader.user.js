1dfd50c2007b85d5f3b784abb9d6c4c6789e185b
mbartnicki80 <mateuszbartnickiq@gmail.com>
2026-03-16 03:13:06 +0100

    fix version

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Margonem Addon Loader
 // @namespace    margonem-addons
-// @version      1.8
+// @version      1.9
 // @match        https://*.margonem.pl/*
 // @grant        GM_xmlhttpRequest
 // @description  addons loader
