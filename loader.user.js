acbc9a153206da0a22b7a8c46e183524506f72bf
mbartnicki80 <mateuszbartnickiq@gmail.com>
2026-03-18 14:30:46 +0100

    version 2.0

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Margonem Addon Loader
 // @namespace    margonem-addons
-// @version      1.10
+// @version      2.0
 // @match        https://*.margonem.pl/*
 // @grant        GM_xmlhttpRequest
 // @description  addons loader
